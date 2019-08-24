b42b44fabfb718c5bef573884a3edd365f6b5ba6
SurakshaRV <43144535+SurakshaRV@users.noreply.github.com>
2019-08-24 10:19:19 +0530

    Update KthSmallest.cpp

--- KthSmallest.cpp
+++ KthSmallest.cpp
@@ -24,10 +24,10 @@ int main(int argc, char **argv)
 		for(int j=i+1;j<n;j++){
 		if(arr[j]<arr[min_i])
 		min_i=j;
+		}
 		int temp=arr[min_i];
 		arr[min_i]=arr[i];
 		arr[i]=temp;
-		}
 	}
 	}
 		end=clock();
