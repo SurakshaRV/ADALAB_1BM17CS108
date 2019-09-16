f0ba32b73ba5d7693164d8050f14684ce382c37b
SurakshaRV <43144535+SurakshaRV@users.noreply.github.com>
2019-09-16 16:40:13 +0530

    Add files via upload

---
+++ bfs.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <queue>
+#include <iostream>
+using namespace std;
+int n;
+//int v[];
+int v[20],adj[20][20],marked[20];
+queue<int> q;
+int c=0;
+void bfs(int ver)
+{
+    c+=1;
+    while(!q.empty())
+    {
+        if(marked[ver]==0)
+        {
+        cout<<q.front();
+        q.pop();
+        cout<<"  ";
+        marked[ver]=1;
+        }
+        else
+            q.pop();
+        for(int i=0;i<n;i++)
+            if(adj[ver][i]==1 && v[i]==0)
+            {c+=1;
+             v[i]=c;
+             q.push(i);
+            }
+      bfs(q.front());
+    }
+
+
+}
+int main()
+{
+  int s;
+    cout<<"Enter number of vertices\n";
+    cin>>n;
+    for(int i=0;i<n;i++)
+        {v[i]=0;marked[i]=0;}
+    cout<<"Enter adjacency matrix\n";
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+            cin>>adj[i][j];
+            cout<<"\nEnter the source vertex: ";
+            cin>>s;
+            q.push(s);
+            bfs(s);
+
+    for(int i=0;i<n;i++)
+        if(v[i]==0)
+        { cout<<"\n";
+          q.push(i);
+          bfs(i);
+        }
+
+    return 0;
+}
